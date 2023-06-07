104a262425517c468fdb79a2dc49bec122ebc1f9
habacuc.eth <habacuc.vera@gmail.com>
2023-06-07 00:07:09 -0600

    tailwind.config update

--- next-app/tailwind.config.js
+++ next-app/tailwind.config.js
@@ -1,4 +1,4 @@
-	/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -10,28 +10,10 @@ module.exports = {
     extend: {
       	colors: {
 			display: ["group-hover"],
-			cata: {
-				900: "#E0A65A95",
-				800: "#bfa98c",
-				700: "#cdaa7e",
-				600: "#daac71",
-				500: "#744b16",
-				400: "#e8ad63",
-				300: "#efae5c",
-				200: "#f6af55",
-				100: "#E0A65A",
-			},
-			mods: {
-				100: "#119195",
-				200: "#0b979b",
-				300: "#119195",
-				400: "#178b8f",
-				500: "#247f82",
-				600: "#024269",
-				700: "#3e6768",
-				800: "#4a5b5c",
-				900: "#011B2F",
-			},
+			bgd: "#101427",
+			cit: "#F3A754",
+			mod: "#008A8A",
+
 		},
         fontFamily: {
 				bau: ['Baumans'],
@@ -40,7 +22,7 @@ module.exports = {
 
 		borderRadius: {
 			'dc': '12px',
-			'dd': '22px',
+			'dd': '40px',
 			'gen': '32px',
 			'med': '20px',
 			'but': '52px',
