5d5fae5a4ebdcbe97da16dfe81c0c8cf20d92e54
habacuc.eth <habacuc.vera@gmail.com>
2023-06-07 00:08:31 -0600

    Wallet update

--- next-app/components/Header.tsx
+++ next-app/components/Header.tsx
@@ -1,22 +1,75 @@
 import Image from 'next/image'
 import Link from 'next/link';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-import SPTM from "../assets/SpaceTimeDAO.svg"
+import SPTM from "../assets/zengo.svg"
+import Navbar from './Navbar';
 
 export default function Header() {
     return (
         <div className="header">
           <div className="wrap">
-          <Link href="/" className="grid items-center">
-            <Image 
-                src={SPTM} 
-                height={50} 
-                width={250}
-                alt="Spacetime DAO"/>
-          </Link>
-            <div className=''>
-              <ConnectButton/>
-            </div>
+              <ConnectButton.Custom>
+                {({
+                  account,
+                  chain,
+                  openAccountModal,
+                  openChainModal,
+                  openConnectModal,
+                  authenticationStatus,
+                  mounted,
+                }) => {
+                  // Note: If your app doesn't use authentication, you
+                  // can remove all 'authenticationStatus' checks
+                  const ready = mounted && authenticationStatus !== 'loading';
+                  const connected =
+                    ready &&
+                    account &&
+                    chain &&
+                    (!authenticationStatus ||
+                      authenticationStatus === 'authenticated');
+                    
+                  return (
+                    <div
+                      {...(!ready && {
+                        'aria-hidden': true,
+                        'style': {
+                          opacity: 0,
+                          pointerEvents: 'none',
+                          userSelect: 'none',
+                        },
+                      })}
+                    >
+                      {(() => {
+                        if (!connected) {
+                          return (
+                            <button className='homeBT fixed bottom-5 left-1/2 -translate-x-1/2 animate-pulse' onClick={openConnectModal} type="button">
+                              Conectar
+                            </button>
+                          );
+                        }
+                      
+                        if (chain.unsupported) {
+                          return (
+                            <button onClick={openChainModal} type="button">
+                              Red incorrecta
+                            </button>
+                          );
+                        }
+                      
+                        return (
+                          <div style={{ display: 'flex', gap: 12 }}>
+                            <Navbar/>
+                            <div className='homeBT'>Gas</div>
+                            <button className='homeBT' onClick={openAccountModal} type="button">
+                              Cuenta
+                            </button>
+                          </div>
+                        );
+                      })()}
+                    </div>
+                  );
+                }}
+              </ConnectButton.Custom>
           </div>
         </div>
     )
