50560d68e8a4b19c43bf9edd3890dbb5a247197a
habacuc.eth <habacuc.vera@gmail.com>
2023-06-08 19:13:27 -0600

    Update on proposal stages

--- next-app/components/DaoProposals.tsx
+++ next-app/components/DaoProposals.tsx
@@ -1,144 +1,80 @@
+import React from 'react';
 import Map from "./Map"
 import Link from "next/link"
 import Image from "next/image"
 import file from "../assets/file.png"
 
+
 export default function daoProposals () {
+    
     return(
-        <div className='h-screen grid items-center'>
+        <div className='h-screen grid items-center '>
             <div className="card0">
                 <div className="propDashboard">
                     <div className="propCard relative">
-                        <div className="bg-white rounded-t-gen grid grid-cols-6 relative">
-                            
-                            <div className="col-span-4 p-3 my-auto">
-                                <div className="italic">Proposal #1</div>
-                                <div className="font-bold text-xl">Test proposal</div>
-                                <div className="italic"> <span className="not-italic text-xl">💻</span>Online event</div>
-                                <div className="text-sm">made on 02/06/2023</div>
-                                <div className="text-sm">made by 0x04...5cC9</div>
-                            </div>
-                            <div className="
-                                col-span-2                                 
-                                border-gray-500/50 
-                                rounded-tr-gen 
-                                rounded-bl-gen grid grid-rows-3"
-                            >
-                                <div className="
-                                    grid 
-                                    items-center text-center 
-                                    font-bold text-xl 
-                                    rounded-tr-gen rounded-bl-gen 
-                                    bg-ssss"
-                                >
-                                    Voting
-                                </div>
-                                <div className="text-center grid grid-cols-3 items-center gap-3 mx-auto p-3 w-full">
-                                    <div className="mx-auto">
-                                        <div className="h-3 w-3 rounded-full bg-gray-500"></div>     
-                                    </div>
-                                    <div className="mx-auto">
-                                        <div className="h-3 w-3 rounded-full bg-gray-500"></div>     
-                                    </div>
-                                    <div className="mx-auto">
-                                        <div className="h-3 w-3 rounded-full bg-gray-500"></div>     
-                                    </div>  
-                                    
-                                </div>
-                                <Link href='/proposal-id'>
-                                    <div className="
-                                        bg-mod hover:bg-mod 
-                                        cursor-pointer hover:text-white 
-                                        grid items-center 
-                                        text-xl px-10 
-                                        rounded-tl-gen"
-                                    > 
-                                        Details
-                                    </div>
-                                </Link>
-                            </div>
-                        </div>
-                        <div className="bg-gray-200/50 rounded-b-gen relative">
-                            <Map/>
-                            <div className="
-                                absolute bottom-0 left-1/2 -translate-x-1/2 w-full 
-                                text-center italic text-white
-                                bg-mod
-                                p-3 rounded-b-dd"
-                            >
-                                Queretaro, Mexico, 20.587834, -100.389245
-                            </div>
-                        </div>
-               
-                    </div>
-                    <div className="propCard relative">
-                        <div className="bg-white rounded-t-gen grid grid-cols-6 relative">
-                            
-                            <div className="col-span-4 p-3 my-auto">
+                        <div className="bg-white rounded-gen grid grid-cols-6 relative">
+                            <div className="col-span-4 p-3">
                                 <div className="italic">Proposal #2</div>
-                                <div className="font-bold text-xl">Clean the park</div>
-                                <div className="italic"> <span className="not-italic text-xl">👷</span>Request work or          maintenance</div>
-                                <div className="text-sm">made on 02/06/2023</div>
-                                <div className="text-sm">made by 0x04...5cC9</div>
+                                <div className="font-bold text-2xl">Clean the park</div>
+                                <div className="italic"> <span className="not-italic text-2xl">👷</span>Request work or maintenance</div>
+                                <div className="">made on 02/06/2023</div>
+                                <div className="">Queretaro, Mexico</div>
                             </div>
                             <div className="
                                 col-span-2                                 
                                 border-gray-500/50 
-                                rounded-tr-gen 
-                                rounded-bl-gen grid grid-rows-3"
+                             
+                                rounded-gen grid grid-rows-3"
                             >
                                 <div className="
                                     grid 
                                     items-center text-center 
                                     font-bold text-xl 
                                     rounded-tr-gen rounded-bl-gen 
-                                    bg-gray-500 text-white"
+                                    bg-cit"
                                 >
                                     On review
                                 </div>
-                                <div className="text-center grid grid-cols-3 items-center gap-3 mx-auto p-3 w-full">
+                                <div className="text-center grid grid-cols-3 items-center gap-3 mx-auto p-3 w-full divide-x divide-gray-300">
                                     <div className="mx-auto">
-                                        <div className="h-3 w-3 rounded-full bg-gray-500"></div>     
+                                        <div className="completed"></div>     
                                     </div>
-                                    <div className="mx-auto">
-                                        <div className="h-3 w-3 rounded-full bg-gray-300"></div>     
+                                    <div className="mx-auto flex gap-1 h-full items-center pl-3">
+                                        <div className="completed"></div>     
+                                        <div className="currentCit"></div>
+                                        <div className="pending"></div>
                                     </div>
-                                    <div className="mx-auto">
-                                        <div className="h-3 w-3 rounded-full bg-gray-300"></div>     
+                                    <div className="mx-auto h-full grid items-center pl-3">
+                                        <div className="pending"></div>     
                                     </div>  
-                                    
                                 </div>
                                 <Link href='/proposal-id-2'>
                                     <div className="
-                                        bg-mod hover:bg-mod 
-                                        cursor-pointer hover:text-white 
+                                        bg-gray-300 hover:bg-gray-500 
+                                        hover:text-white 
+                                        cursor-pointer 
                                         grid items-center 
-                                        text-xl px-10 
-                                        rounded-tl-gen"
+                                        text-xl text-center 
+                                        rounded-tl-gen
+                                        rounded-br-gen"
                                     > 
                                         Details
                                     </div>
                                 </Link>
                             </div>
                         </div>
-                        <div className="bg-gray-200/50 rounded-b-gen relative">
-                            <Map/>
-                            <div className="
-                                absolute bottom-0 left-1/2 -translate-x-1/2 w-full 
-                                text-center italic text-white
-                                bg-mod
-                                p-3 rounded-b-dd"
-                            >
-                                Queretaro, Mexico, 20.587834, -100.389245
-                            </div>
-                        </div>
-               
                     </div>
                     <Link href='/newProposal'>
-                        <div className="newProp h-[375px] rounded-gen text-white ">+ Add a new proposal</div>
+                        <div className="newProp ">+ Add a new proposal</div>
                     </Link>
                 </div>
+                <div className='grid h-full col-span-2 p-3 gap-3 font-bau text-2xl'>
+                    <div className='propBT'>Ver todas</div>
+                    <div className='propBT'>Mis propuestas</div>
+                    <div className='propBT'>Por tipo</div>
+                </div>
             </div>
+
         </div>
     )
 }
