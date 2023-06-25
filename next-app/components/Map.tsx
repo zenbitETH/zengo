721b495b28a0fbde63b62d322b361715f8528e04
habacuc.eth <habacuc.vera@gmail.com>
2023-06-24 21:48:34 -0600

    map is messing

--- next-app/components/Map.tsx
+++ next-app/components/Map.tsx
@@ -40,17 +40,9 @@ const Map: NextPage = () => {
 
   return (
     <div className='GMAPS'>
-      <GoogleMap
-        options={mapOptions}
-        zoom={14}
-        center={mapCenter}
-        mapTypeId={google.maps.MapTypeId.ROADMAP}
-        /*mapContainerStyle={containerStyle}*/
-        
-        onLoad={() => console.log('Map Component Loaded...')}    
-      >
-          <MarkerF position={mapCenter} onLoad={() => console.log('Marker Loaded')} />
-    </GoogleMap>
+  
+/
+    
     </div>
   );
 };
