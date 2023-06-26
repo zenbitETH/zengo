63ec2bf39891f1c336cb29f0abadec2b7ae40627
habacuc.eth <habacuc.vera@gmail.com>
2023-06-26 17:32:18 -0600

    margin on incons

--- next-app/components/Header.tsx
+++ next-app/components/Header.tsx
@@ -45,14 +45,14 @@ export default function Header() {
                           return (
                             <div className=''>
                               
-                              <div className='fixed left-1/2 -translate-x-1/2 top-5 grid grid-cols-2 gap-5 backdrop-blur-md w-full -mt-5 pt-5'>
+                              <div className='fixed left-1/2 -translate-x-1/2 top-5 grid grid-cols-2 gap-3 backdrop-blur-md w-full -mt-5 pt-5'>
                                 
                                 <Link href="https://certamentransparencia.org.mx/">
                                   <div className='text-center'>
                                     <Image height={50} width={100} src={cit} alt='Certamen de Innovación en transparencia 2023' className='cursor-pointer hover:opacity-100 rounded-full opacity-50'/>
                                   </div>
                                 </Link>
-                                <div className='grid grid-cols-3 gap-10 grid items-center mx-auto'>
+                                <div className='grid grid-cols-3 gap-3 grid items-center mx-auto'>
                                   <Link href="https://twitter.com/zenbitMX">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" className='icons'  viewBox="0 0 16 16"> <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/> </svg>
                                   </Link>
