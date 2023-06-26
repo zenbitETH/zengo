624e6703e6b7e2c6cc9b40a02102d8b3be11a1fa
habacuc.eth <habacuc.vera@gmail.com>
2023-06-26 11:15:12 -0600

    fix on carousel buttons

--- next-app/components/Carousel.tsx
+++ next-app/components/Carousel.tsx
@@ -97,7 +97,7 @@ const Carousel: React.FC = () => {
         className="carBT right-0"
         onClick={goToNext}
       >
-        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" className='fill-bgd/50 hover:fill-white'  viewBox="0 0 16 16"> <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/> </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32"   viewBox="0 0 16 16"> <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/> </svg>
       </button>
     </div>
   );
