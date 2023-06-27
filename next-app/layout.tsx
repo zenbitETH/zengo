bea7504d27c1aba5836fcdedb3138ca812cf74de
habacuc.eth <habacuc.vera@gmail.com>
2023-06-26 23:18:19 -0600

    metadata file

---
+++ next-app/layout.tsx
@@ -0,0 +1,8 @@
+import { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Zengo: Presupuesto descentralizado',
+  description: 'Aplicación de transparencia colaborativa que facilita el registro, verificación, fondeo y seguimiento de propuestas de gobernanza urbana',
+}
+ 
+export default function Page() {}
