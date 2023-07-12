6ecaa1a92e5dd300b906ae9f3906a22293a7e1b0
habacuc.eth <habacuc.vera@gmail.com>
2023-07-11 21:48:47 -0600

    fix on progress bar

--- next-app/pages/newProposal.tsx
+++ next-app/pages/newProposal.tsx
@@ -95,7 +95,7 @@ const Form = () => {
   }, [triggerTx])
 
   return (
-    <div className="from-cit/50 to-cit bg-gradient-to-t h-screen grid items-center text-center mx-auto px-3 py-20 xl:py-0">
+    <div className="from-cit/90 to-cit bg-gradient-to-t h-screen grid items-center text-center mx-auto px-3 py-20 xl:py-0">
       <form className='formCard'>
         {currentStep === 1 && (
           <div className=''>
