14d4693153f154173f6cf4eacca13d6ab58ccb12
habacuc.eth <habacuc.vera@gmail.com>
2023-07-13 12:15:07 -0600

    Gas and prop buttons realocated

--- next-app/components/Header.tsx
+++ next-app/components/Header.tsx
@@ -95,9 +95,6 @@ export default function Header() {
                         return (
                           <div style={{ display: 'flex', gap: 12 }}>
                             {/*<Navbar/>*/}
-                            <Link href='/gasStation'>
-                              <div className='homeBT'>Gas</div>
-                            </Link>
                             <button className='homeBT' onClick={openAccountModal} type="button">
                               Cuenta
                             </button>
