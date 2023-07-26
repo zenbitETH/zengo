644c65813553d217c4f04fe203eac3b42935ffb3
habacuc.eth <habacuc.vera@gmail.com>
2023-07-25 18:40:44 -0600

    probly is no the marker

--- next-app/components/Map.tsx
+++ next-app/components/Map.tsx
@@ -44,10 +44,10 @@ const Map: NextPage = () => {
         zoom={14}
         center={mapCenter}
         mapTypeId={google.maps.MapTypeId.ROADMAP}
-        mapContainerStyle={containerStyle}
+       // *mapContainerStyle={containerStyle}/
         onLoad={(map) => setMap(map)}    
       >
-        {/*<MarkerF position={mapCenter} onLoad={() => console.log('Marker Loaded')} />*/}
+        <MarkerF position={mapCenter} onLoad={() => console.log('Marker Loaded')} />
     </GoogleMap>
   );
 };
