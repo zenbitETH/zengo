48470e2ef11b3e05a0014dd7e55878ad3a00b474
habacuc.eth <habacuc.vera@gmail.com>
2023-07-25 18:29:38 -0600

    map parent fix

--- next-app/components/Form3.tsx
+++ next-app/components/Form3.tsx
@@ -35,9 +35,9 @@ export default function Form3 (props: any) {
                 <div className='formLabel'>Sube tu evidencia</div>
                 <div className="flex justify-center items-center w-full h-full pb-10">
                     <label className="formFile">
-                        <div className="flex flex-col justify-center items-center pt-5 pb-6">
+                        <div className="flex flex-col justify-center items-center pt-5 pb-6 font-exo">
                             <svg aria-hidden="true" className="mb-3 w-10 h-10 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-                            <p className="mb-2 text-sm  text-white "><span className="font-semibold">Toca para elegir tu archivo</span> o arrastralo y suelta aquí</p>
+                            <p className="mb-2 text-sm  text-white "><span className="font-semibold ">Toca para elegir tu archivo</span> o arrastralo y suelta aquí</p>
                         </div>
                         <input id="dropzone-file" type="file" className="hidden" onChange={handleEvidenceForm} />
                     </label>
