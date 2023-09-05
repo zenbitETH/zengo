6f7ccacfff191b39d5a17c32cfad88ff242fdff4
Marcos Iglesias <markkos89@gmail.com>
2023-09-05 04:18:26 -0300

    feat: pantalla poap

--- apps/application/pages/onboarding/mods/poap.tsx
+++ apps/application/pages/onboarding/mods/poap.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Image from "next/image";
 import { useAddress } from "@thirdweb-dev/react";
 import { useGlobalCycleStageState } from "@/contexts/GlobalStageCycleContext";
@@ -8,47 +8,107 @@ const eventIdUsed = process.env.NEXT_PUBLIC_POAP_MODERATOR_EVENT_ID as string;
 
 const PoapModeratorsPage = () => {
   const address = useAddress();
-  const { claimPoap, addressHasPoap } = useGlobalCycleStageState();
+  const { claimPoap, poapScan, addressHasPoap } = useGlobalCycleStageState();
+
+  useEffect(() => {
+    if (address) {
+      poapScan(address, eventIdUsed);
+    }
+  }, [address]);
 
   const handleClaimPoap = async () => {
     if (address) {
-      // setIsModal2Open(true);
       claimPoap(address, eventIdUsed);
-      // setIsModal2Open(false);
     }
   };
 
   return (
     <div className="overflow-hidden text-center h-screen grid items-center relative px-32">
       <div className="font-exo p-3 gap-3 grid xl:grid-cols-4 items-center px-5 relative">
-        <div className="">
-          <Image
-            src={"/assets/poaptest.png"}
-            height={250}
-            width={250}
-            alt="onboarding POAP"
-            className="rounded-full mx-auto"
-          />
-        </div>
-        <div className="xl:col-span-3 grid gap-3 text-2xl text-justify px-5">
-          Este POAP certifica tu participación en el evento de introducción para
-          ciudadanos y te permite crear tu primer propuesta de gobernanza sin
-          gas⛽.
-        </div>
         <div className="grid items-center xl:col-span-4">
           {!addressHasPoap ? (
-            <button
-              className={"homeBT mt-5 w-fit mx-auto"}
-              onClick={handleClaimPoap}
-            >
-              Obtener POAP
-            </button>
-          ) : (
-            <Link href="/onboarding/mods/register">
-              <button className="homeBT mt-5 w-fit mx-auto">
-                Listo! Solicitar mi rango de moderador
+            <>
+              <div className="POAPmargin">
+                <div className="">
+                  <Image
+                    src={"/assets/POAPMod.png"}
+                    height={250}
+                    width={250}
+                    alt="onboarding POAP"
+                    className="rounded-full mx-auto animate-pulse"
+                  />
+                </div>
+                <div className=" xl:text-xl text-justify bg-mod/70 py-3 px-5 w-full text-white">
+                  <ul className="grid gap-3">
+                    <li>
+                      <span className="text-2xl">🎖️</span> Este POAP certifica
+                      tu participación en el evento de introducción para
+                      Moderadores de Zengo.
+                    </li>
+                    <li>
+                      <span className="text-2xl">⛽</span>
+                      Además te permite participar en este ciclo de gobernanza
+                      sin gas.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              <button
+                className={"homeBT mt-5 w-fit mx-auto"}
+                onClick={handleClaimPoap}
+              >
+                Obtener POAP
               </button>
-            </Link>
+            </>
+          ) : (
+            <>
+              <div className="POAPmargin">
+                <div className="">
+                  <Image
+                    src={"/assets/POAPMod.png"}
+                    height={250}
+                    width={250}
+                    alt="onboarding POAP"
+                    className="rounded-full mx-auto"
+                  />
+                </div>
+                <div className="grid gap-3">
+                  <div className="text-2xl font-bau">
+                    ¡Ahora tienes el POAP de moderadores!
+                  </div>
+                  <div className="text-sm xl:text-lg font-bau">
+                    Septiembre, 2023 /
+                    <span className="cursor-pointer hover:text-mod">
+                      {" "}
+                      <Link href="https://app.poap.xyz/token/6741694">
+                        Ver POAP
+                      </Link>
+                    </span>
+                  </div>
+                  <div className="xl:text-lg text-justify xl:px-10 bg-mod/70 px-5 py-3 text-white">
+                    <ul className="grid gap-3">
+                      <li>
+                        <span className="text-2xl">🎖️</span> Participaste en el
+                        evento de introducción y ahora puedes registrarte como
+                        moderador.
+                      </li>
+                      <li>
+                        <span className="text-2xl">⛽</span>
+                        El POAP te otorga gas para las interacciones que
+                        realices en Zengo a lo largo de este ciclo de
+                        gobernanza.
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <Link href="/onboarding/mods/register">
+                <button className="homeBT mt-5 w-fit mx-auto">
+                  Listo! Solicitar mi rango de moderador
+                </button>
+              </Link>
+            </>
           )}
         </div>
       </div>
